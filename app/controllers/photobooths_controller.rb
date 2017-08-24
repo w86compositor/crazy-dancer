704179965290054621af844933797e570918e7f5
Waldo <w86ccompositor@gmail.com>
2017-08-24 15:19:25 -0400

    Added images for user profile page, and Bootstrap 4 for our project

---
+++ app/controllers/photobooths_controller.rb
@@ -0,0 +1,10 @@
+class PhotoboothsController < ApplicationController
+  def new
+  end
+
+  def edit
+  end
+
+  def delete
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
   def new
-    @user = User.create(name: "Stephanie", email: "stephaniegrillo329@gmail.com")
+    
   end
 end
