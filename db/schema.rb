d8fdfade34ca237aaa9f9d374b6ab08e64df7d91
Daniel Philpot <Dphil300@gmail.com>
2017-08-26 14:17:22 -0400

    complete index page and pushing changes

--- db/schema.rb
+++ db/schema.rb
@@ -28,10 +28,7 @@ ActiveRecord::Schema.define(version: 20170826001758) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", id: false, force: :cascade do |t|
-    t.text "created_at"
-    t.text "updated_at"
-    t.integer "id"
+  create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
